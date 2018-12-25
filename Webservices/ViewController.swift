9c92f1d7cdd031b4b6a842ffba562211104f5474
Abdallh <bodaa_roka_2010@yahoo.com>
2018-12-25 20:06:27 +0200

    Initial Commit

---
+++ Webservices/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Webservices
+//
+//  Created by Abdallh on 12/25/18.
+//  Copyright © 2018 Abdullah Tarek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
