d06e32410c32e441b76a9ed8173d01323324b9d5
ahmed abu elregal <ahmedabuelregal@192.168.181.130>
2019-02-02 18:55:56 -0800

    a

---
+++ Webservices/DeptsTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  DeptsTableViewCell.swift
+//  Webservices
+//
+//  Created by Abdallh on 12/25/18.
+//  Copyright © 2018 Abdullah Tarek. All rights reserved.
+//
+
+import UIKit
+
+class DeptsTableViewCell: UITableViewCell {
+
+    
+    @IBOutlet var titleLbl : UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Webservices/ViewController.swift
+++ Webservices/ViewController.swift
@@ -7,14 +7,121 @@
 //
 
 import UIKit
+import Alamofire
+import  SwiftyJSON
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate{
+    
+    
+    
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return array.count
+    }
+    
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 
+        
+        let cell   = tableview.dequeueReusableCell(withIdentifier: "DeptsTableViewCell") as!  DeptsTableViewCell
+        
+        let dic  :  [String  : Any]   = array[indexPath.row]
+        
+        cell.titleLbl.text =  dic["name"] as! String
+        return cell
+    }
+    
+    var array : [  [String  : Any]   ]  = []
+    let url = "http://luqia.net/api/depts"
+    
+    @IBOutlet weak var tableview: UITableView!
+    
     override func viewDidLoad() {
+        
+        
+        tableview.dataSource = self
+        tableview.delegate = self
+        
+        
+        
+        
+        
+        
         super.viewDidLoad()
+        
+        Alamofire.request(url, method: .get, parameters: nil, encoding: URLEncoding.default, headers: nil)
+        
+            .responseJSON { response in
+                
+                
+                
+                
+                switch response.result {
+                    
+                    
+                case .failure(let error)  :
+                    print(error)
+                    
+                case .success(let value):
+                    
+                    
+                    
+                    let json =  JSON.init(value)
+                    
+                    //
+                    let status = json["status"].string
+                    print("status is ", status)
+                    
+                    
+                    
+                    let data  = json["data"].array
+
+                    
+                    for item in data!
+                    {
+                        
+                        
+                        let dic  = [ "id " : item["id"].int   , "name" : item["name"] .string] as! [String : Any]
+                        self.array.append(dic)
+                        
+                        
+                    }
+                    
+                    self.tableview.reloadData()
+                    
+                    
+                    print( "IDSIDS" , self.array)
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+//                    let arr = json["data"].array
+//                    for item in arr {
+//
+//                      let value = item
+//
+//
+//                    }
+//
+                    
+                    
+                    
+                }
+                
+                
+        }
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
 
 }
+
+
+
 
